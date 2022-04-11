4462b0484fe73a42576ab0ab802ee3527fb1a06b
3rix4 <3rix40@gmail.com>
2022-04-10 22:02:16 -0500

    added film freeway button and adjusted main blurb

--- src/index.js
+++ src/index.js
@@ -3,14 +3,25 @@ import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+function FilmButton(props){
+    return (
+        <a href="https://filmfreeway.com/festivals/66443?utm_campaign=The+Northern+Pocket+Film+Festival&utm_medium=External&utm_source=Submission+Button" target="_blank" title="Click to submit on FilmFreeway">
+            <img className="filmButton" src="https://storage.googleapis.com/filmfreeway-assets/submission_buttons/v2/sm_submission_btn@2x-sky-gradient.png" title="Click to submit on FilmFreeway"/>
+        </a>
+    );
+}
+
 function Body(props){
     return (
         <div className="txarea">
             <h3>A Minnesota Film Festival</h3>
             <p>
-                Thanks for your interest in the Northern Pocket Film Festival, hosted at the University of Minnesota, Twin Cities. <br/><br/>
-                This website is currently under construction. Please come back soon for more information or to submit your film!
+                The Northern Pocket is a Film Festival, hosted at the University of Minnesota, Twin Cities. <br/><br/>
+                The event will be held in the Toaster in the basement of Walter Library at UMN on May 12th.
+                Submissions will be open until May 1st, and accepted by May 7th.
+                If you're interested in submitting your film, please use the button below!
             </p>
+            <FilmButton/>
         </div>
     )
 }
